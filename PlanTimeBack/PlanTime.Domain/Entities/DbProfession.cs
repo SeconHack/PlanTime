eca6b663c787e55b7886dc8578e083d23fa26595
Lygin04 <kosty.lygin.58@yandex.ru>
2025-04-11 17:37:57 +0300

    Add ProfessionController.cs

--- PlanTimeBack/PlanTime.Domain/Entities/DbProfession.cs
+++ PlanTimeBack/PlanTime.Domain/Entities/DbProfession.cs
@@ -16,12 +16,12 @@ public class DbProfession
     public string ProfessionName { get; set; }
     
     /// <summary>
-    /// Количество дней отпуска.
+    /// Получает или задает количество дней отпуска.
     /// </summary>
     public int CountVacationDays { get; set; } = 28;
     
     /// <summary>
-    /// Сколько сотрудников данной должности должно оставаться работать.
+    /// Получает или задает сколько сотрудников данной должности должно оставаться работать.
     /// </summary>
     public int CountInterchangeable  { get; set; }
 }
